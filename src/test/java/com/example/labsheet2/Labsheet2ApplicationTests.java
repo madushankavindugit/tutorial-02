cfc768c9bf5c182d33ef569397a98ba472bc924e
PS/2020/089 - MADUSHAN G.K.K <madusha-ps20089@kln.ac.lk>
2024-11-05 17:20:37 +0530

    tutorial02

---
+++ src/test/java/com/example/labsheet2/Labsheet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labsheet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labsheet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
